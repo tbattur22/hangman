27358c849d6d18800200a033972148b789c7b555
Battur Tugsgerel <tbattur22@yahoo.com>
2025-07-06 12:54:50 -0500

    Bumped version for adding debug

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hangman.MixProject do
   def project do
     [
       app: :hangman,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
